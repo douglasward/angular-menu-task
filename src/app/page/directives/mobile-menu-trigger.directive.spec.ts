d707709b3f95d5c98e6fcf0782e579ab25e073ac
Sven Meyer <sm@radiosphere.com>
2020-07-08 10:32:09 +0200

    Added spec test for MobileMenuTriggerDirective

--- src/app/page/directives/mobile-menu-trigger.directive.spec.ts
+++ src/app/page/directives/mobile-menu-trigger.directive.spec.ts
@@ -1,9 +1,41 @@
-import { MobileMenuTriggerDirective } from './mobile-menu-trigger.directive';
+import {MobileMenuTriggerDirective} from './mobile-menu-trigger.directive';
 import {ApplicationStateService} from '../../shared/services/application-state.service';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {Component, DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
+
+@Component({
+  template: '<span appMobileMenuTrigger></span>'
+})
+class DummySpanComponent {
+}
 
 describe('MenuTriggerDirective', () => {
+  let spanComponentFixture: ComponentFixture<DummySpanComponent>;
+  let spanElement: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [MobileMenuTriggerDirective, DummySpanComponent]
+    });
+
+    spanComponentFixture = TestBed.createComponent(DummySpanComponent);
+    spanElement = spanComponentFixture.debugElement.query(By.css('span'));
+  });
+
   it('should create an instance', () => {
     const directive = new MobileMenuTriggerDirective(new ApplicationStateService());
     expect(directive).toBeTruthy();
+  });
+
+  it('should change mobile menu open state in app state after click', () => {
+    const appState = TestBed.inject(ApplicationStateService);
+
+    expect(appState.mobileMenuOpen$.getValue()).toBe(false);
+
+    spanElement.triggerEventHandler('click', null);
+    spanComponentFixture.detectChanges();
+
+    expect(appState.mobileMenuOpen$.getValue()).toBe(true);
   });
 });
